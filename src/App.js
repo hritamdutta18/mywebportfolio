fa4b2a216396cd2e5ddae1f87b398c6ebcb4a557
Hritam Dutta <hritamloyola10@gmail.com>
2022-01-09 02:16:08 +0530

    Miscellaneous

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { Route, Switch } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+import { lightTheme } from "./components/Themes"
+import GlobalStyles from "./globalStyles"
+
+// Components Imported
+import Main from './components/Main'
+import AboutPage from './components/AboutPage'
+import ExperiencePage from './components/ExperiencePage'
+import ProjectPage from './components/ProjectPage'
+import SkillsPage from './components/SkillsPage'
+
+
 function App() {
-  return <div>
-    App File
-    </div>
+  return <>
+
+  < GlobalStyles />
+
+    <ThemeProvider theme= { lightTheme }>
+      <Switch>
+        <Route exact path= "/" component= { Main } />
+        <Route exact path= "/about" component= { AboutPage } />
+        <Route exact path= "/experience" component= { ExperiencePage } />
+        <Route exact path= "/projects" component= { ProjectPage } />
+        <Route exact path= "/skills" component= { SkillsPage } />
+      </Switch>
+    </ThemeProvider>
+    
+  </>
     
 }
 
