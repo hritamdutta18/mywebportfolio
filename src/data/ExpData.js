71d692e5901b8ddd668ef61d63005798b8ccdf60
Hritam Dutta <hritamloyola10@gmail.com>
2022-01-18 13:39:02 +0530

    Data for Experience Page

---
+++ src/data/ExpData.js
@@ -0,0 +1,37 @@
+export const Experience = [
+    {
+        id: 1,
+        name: "IEEE Industry Applications Society - VIT",
+        position: "Core Committee Member (Technical)",
+        date: "January 2021 - Present",
+        imgSrc:"https://ias.ieee.org/images/files/template/ias-logo-shadowbg.png"
+    },
+    {
+        id: 2,
+        name: "Institution of Engineers (India) - VIT",
+        position: "Human Resources Head",
+        date: "May 2021 - Present (CC Member since January 2021)",        
+        imgSrc:"https://www.sandipuniversity.edu.in/images/iei.jpg"
+    },
+    {
+        id: 3,
+        name: "Unschool",
+        position: "Sales & Marketing Intern",
+        date: "Februrary 2021 - March 2021",
+        imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNjxETTnQmT3GvW0dFjeLlEFGeK2vV1Mpg3Cx1GO_atlKbEvaK1v07peBhR7wK2Mr7j1g&usqp=CAU"
+    },
+    {
+        id: 4,
+        name: "PROTAL",
+        position: "Frontend (React) Developer",
+        date:"August 2021 - November 2021",
+        imgSrc:"https://protal.netlify.app/static/media/PROTAL_LOGO3.46253832.svg"
+    },
+    {
+        id: 5,
+        name: "Fidelity Investments",
+        position: "SDE Intern",
+        date: "June 2022 - July 2022",
+        imgSrc:"https://blog.logomyway.com/wp-content/uploads/2021/04/fidelity-logo-PNG.png"
+    }  
+] 
