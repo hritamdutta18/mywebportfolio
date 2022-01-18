674511f9aa358517e8dd27419f232674f281fad1
Hritam Dutta <hritamloyola10@gmail.com>
2022-01-18 13:40:19 +0530

    Experience Page 1.0

--- src/components/ExperiencePage.js
+++ src/components/ExperiencePage.js
@@ -1,10 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components'
+import img from '../assets/Images/experience-wallpaper.jpeg'
+import LogoComponent from '../subComponents/LogoComponent'
+import HomeButton from '../subComponents/HomeButton'
+import SocialIcons from '../subComponents/SocialIcons'
+import { Experience } from '../data/ExpData'
+import ExpComponent from './ExpComponent'
+import AnchorComponent from '../subComponents/AnchorComponent'
+
+
+const MainContainer= styled.div`
+    background-image: url(${img});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center;
+    width: 100vw;
+    height: auto;
+`
+
+const Container= styled.div`
+    background-color: ${props => `rgba(${props.theme.bodyRgba}, 0.8)`};
+    width: 100%;
+    height: auto;
+    position: relative;
+    padding-bottom: 5rem;
+`
+
+const Center= styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 10rem;
+`
+
+const Grid= styled.div`
+    display: grid;
+    grid-template-column: repeat (2, minmax(calc(10rem + 15vw), 1fr));
+    grid-gap: calc(1rem + 2vw);
+`
+
 
 const ExperiencePage = () => {
+
+    const [numbers, setNumbers]= useState(0);
+
+    useEffect(() => {
+        let num= (window.innerHeight - 70)/30;
+        setNumbers(parseInt(num));
+    }, [])
+
     return (
-        <div>
-            
-        </div>
+        <MainContainer>
+            <Container>
+                <LogoComponent  />
+                <HomeButton />
+                <SocialIcons />
+                <AnchorComponent numbers= {numbers}/>
+                <Center>
+                    <Grid>
+                        {
+                            Experience.map(exp => {
+                                return <ExpComponent key= {exp.id} exp= {exp} />
+                            })
+                        }
+                    </Grid>
+                </Center>
+            </Container>
+        </MainContainer>
     )
 }
 
