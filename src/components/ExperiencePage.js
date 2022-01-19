a87dcdbd8be657d73cba48afdbe249941538528f
Hritam Dutta <hritamloyola10@gmail.com>
2022-01-19 12:20:16 +0530

    Experience Page V1.1

--- src/components/ExperiencePage.js
+++ src/components/ExperiencePage.js
@@ -18,7 +18,6 @@ const MainContainer= styled.div`
     width: 100vw;
     height: auto;
 `
-
 const Container= styled.div`
     background-color: ${props => `rgba(${props.theme.bodyRgba}, 0.8)`};
     width: 100%;
@@ -26,20 +25,17 @@ const Container= styled.div`
     position: relative;
     padding-bottom: 5rem;
 `
-
 const Center= styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     padding-top: 10rem;
 `
-
 const Grid= styled.div`
     display: grid;
-    grid-template-column: repeat (2, minmax(calc(10rem + 15vw), 1fr));
-    grid-gap: calc(1rem + 2vw);
+    grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
+    grid-gap: calc(2rem + 2vw);
 `
-
 
 const ExperiencePage = () => {
 
