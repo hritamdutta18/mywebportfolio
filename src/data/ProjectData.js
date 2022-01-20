6acc77d3d83d743134347ec74c7ba0885eb96db6
Hritam Dutta <hritamloyola10@gmail.com>
2022-01-21 03:00:18 +0530

    Project Page Data

---
+++ src/data/ProjectData.js
@@ -0,0 +1,60 @@
+export const Project = [
+    {
+        id:1,
+        name:"BeeResume - An Automated Resume Builder",
+        description:"BeeResume gives the user an automatically generated pre-designed resume, using a Multi-Step form.",
+        tags:["react","bootstrap","node"],        
+        demo:"https://bee-resume.herokuapp.com/",
+        github:"https://github.com/hritamdutta18/bee-resume"
+    },
+    {
+        id:2,
+        name:"Diary",
+        description:"A globally accessible personal diary that the writer can access and edit from anywhere.",
+        tags:["react","node","mongodb","bootstrap","express"],        
+        demo:"https://diary-iwp.herokuapp.com/",
+        github:"https://github.com/abhitcr1/IWPproject"        
+    },
+    {
+        id:3,
+        name:"SGGES '21 Website",
+        description:"Website for the Virtual International Conference on Smart Grid and Green Energy Systems (SGGES '21), which was organized by VIT Vellore and technically sponsored by IEEE IAS VIT Vellore.",
+        tags:["html","css","bootstrap","javascript"],        
+        demo:"https://vit.ac.in/SGGES-21/Home_page/index.html",
+        github:"https://github.com/ieee-ias-vit/SGGES-21/"
+    },
+    {
+        id:4,
+        name:"IEEE IAS Website",
+        description:"Official website of IEEE IAS VIT Vellore, made by members of the chapter.",
+        tags:["html","css","bootstrap","javascript","firebase"],        
+        demo:"https://ieeeiasvit.in/",
+        github:"https://github.com/hritamdutta18/IEEE-IAS-Website-2021"       
+    },
+    {
+        id:5,
+        name:"ZooMath",
+        description:"ZooMath is a fun learning based web application that allows children to learn and practice multiplication tables and attend quizzes based on them.",
+        tags:["html","css","javascript","bootstrap"],        
+        demo:"https://hritamdutta18.github.io/Hackulus_Team_DASH/home/page.html",
+        github:"https://github.com/hritamdutta18/Hackulus_Team_DASH"        
+    },
+    {
+        id:6,
+        name:"Diary in C",
+        description:"A simple personal diary application made using C language.",
+        tags:["c","cmd"],        
+        demo:"https://github.com/hritamdutta18/diary",
+        github:"https://github.com/hritamdutta18/diary"
+        
+    },  
+    {
+        id:7,
+        name:"CoviPrec",
+        description:"This is my first ever website ! It enlists a few articles & precautions regarding COVID-19, along with a form for contact details. No back-end functionalities are included.",
+        tags:["html","css","javascript"],        
+        demo:"https://hritamdutta18.github.io/covid_prec/webpage",
+        github:"https://github.com/hritamdutta18/covid_prec"
+        
+    }  
+] 
