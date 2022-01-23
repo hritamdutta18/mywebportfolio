257a8c28eb472e28225e3a42918d05b0a5f0896b
Hritam Dutta <hritamloyola10@gmail.com>
2022-01-23 16:52:44 +0530

    Education Data Added & Experience Data Modiefied

---
+++ src/data/EducationData.js
@@ -0,0 +1,30 @@
+import vit from '../assets/Images/education/VIT-logo.png'
+import loyola from '../assets/Images/education/Loyola-logo.png'
+
+
+export const Education = [
+    {
+        id: 1,
+        name: "Vellore Institute of Technology, Vellore",
+        branch: "B.Tech CSE with specialization in IoT",
+        marks: "CGPA - 9.12",
+        date: "July 2019 - Present",
+        imgSrc: vit
+    },
+    {
+        id: 2,
+        name: "Loyola School, Jamshedpur",
+        branch: "ISC",
+        marks: "95.75%",
+        date: "April 2016 - May 2018",        
+        imgSrc: loyola
+    },
+    {
+        id: 3,
+        name: "Loyola School, Jamshedpur",
+        branch: "ICSE",
+        marks: "96%",
+        date: "March 2005 - March 2016",
+        imgSrc: loyola
+    }
+] 
--- src/data/ExpData.js
+++ src/data/ExpData.js
@@ -1,37 +1,44 @@
+import ieee from '../assets/Images/experience/ieee-ias-logo.png'
+import iei from '../assets/Images/experience/iei-logo.jpg'
+import unschool from '../assets/Images/experience/unschool-logo.png'
+// import fidelity from '../assets/Images/experience/fidelity-logo.png'
+import protal from '../assets/Images/experience/protal-logo.svg'
+
+
 export const Experience = [
     {
         id: 1,
         name: "IEEE Industry Applications Society - VIT",
         position: "Core Committee Member (Technical)",
         date: "January 2021 - Present",
-        imgSrc:"https://ias.ieee.org/images/files/template/ias-logo-shadowbg.png"
+        imgSrc: ieee
     },
     {
         id: 2,
         name: "Institution of Engineers (India) - VIT",
         position: "Human Resources Head",
         date: "May 2021 - Present (CC Member since January 2021)",        
-        imgSrc:"https://www.sandipuniversity.edu.in/images/iei.jpg"
+        imgSrc: iei
     },
     {
         id: 3,
         name: "Unschool",
         position: "Sales & Marketing Intern",
         date: "Februrary 2021 - March 2021",
-        imgSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNjxETTnQmT3GvW0dFjeLlEFGeK2vV1Mpg3Cx1GO_atlKbEvaK1v07peBhR7wK2Mr7j1g&usqp=CAU"
+        imgSrc: unschool
     },
     {
         id: 4,
         name: "PROTAL",
         position: "Frontend (React) Developer",
         date:"August 2021 - November 2021",
-        imgSrc:"https://protal.netlify.app/static/media/PROTAL_LOGO3.46253832.svg"
-    },
-    {
-        id: 5,
-        name: "Fidelity Investments",
-        position: "SDE Intern",
-        date: "June 2022 - July 2022",
-        imgSrc:"https://blog.logomyway.com/wp-content/uploads/2021/04/fidelity-logo-PNG.png"
-    }  
+        imgSrc: protal
+    }
+    // {
+    //     id: 5,
+    //     name: "Fidelity Investments",
+    //     position: "SDE Intern",
+    //     date: "June 2022 - July 2022",
+    //     imgSrc: fidelity
+    // }  
 ] 
