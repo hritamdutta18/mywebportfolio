804f0ce0f7de6e2404c6c9903775cef9f2e70dba
Hritam Dutta <hritamloyola10@gmail.com>
2022-01-25 03:16:10 +0530

    Implemented Education Section & BG Titles

--- src/components/ExperiencePage.js
+++ src/components/ExperiencePage.js
@@ -1,42 +1,88 @@
-import React, { useEffect, useState } from 'react'
+import React, { useRef } from 'react'
 import styled from 'styled-components'
-import img from '../assets/Images/experience-wallpaper.jpeg'
+import img2 from '../assets/Images/experience-wallpaper.jpeg'
+import img1 from '../assets/Images/education-wallpaper.jpg'
 import LogoComponent from '../subComponents/LogoComponent'
 import HomeButton from '../subComponents/HomeButton'
 import SocialIcons from '../subComponents/SocialIcons'
 import { Experience } from '../data/ExpData'
+import { Education } from '../data/EducationData'
 import ExpComponent from './ExpComponent'
-import AnchorComponent from '../subComponents/AnchorComponent'
+import EduComponent from './EduComponent'
 import BackgroundTitle from '../subComponents/BackgroundTitle'
 import { motion } from 'framer-motion'
 
 
 const MainContainer= styled(motion.div)`
-    background-image: url(${img});
     background-size: cover;
     background-repeat: no-repeat;
     background-attachment: fixed;
     background-position: center;
     width: 100vw;
-    height: auto;
+    height: 100vh;
+    position: relative;
+    scroll-snap-type: y mandatory;    
+    overflow-y: scroll;
+
+    &>:nth-child(1){
+        background-image: url(${img1});
+        z-index: 1;
+    }
+    &>:nth-child(2){
+        background-image: url(${img2});
+    }
+    &>::-webkit-scrollbar {
+        display: none;
+    }
 `
-const Container= styled.div`
+const Container1= styled.div`
     background-color: ${props => `rgba(${props.theme.bodyRgba}, 0.8)`};
     width: 100%;
-    height: auto;
+    height: 100vh;
     position: relative;
     padding-bottom: 5rem;
+    overflow-y: scroll;    
+    scroll-snap-align: start;
+    background-blend-mode: lighten;
+
+    .center-edu{
+        padding-top: 3rem;
+    }
+`
+const Container2= styled.div`
+    background-color: ${props => `rgba(${props.theme.bodyRgba}, 0.8)`};
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    padding-bottom: 5rem;
+    overflow-y: scroll;    
+    scroll-snap-align: start;
+    background-blend-mode: lighten;
+
+    .center-exp{
+        padding-top: 10rem;
+    }
 `
 const Center= styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    padding-top: 10rem;
+    position: relative;
+    width: 100%;
+    height: 100%;
 `
 const Grid= styled.div`
     display: grid;
-    grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
-    grid-gap: calc(1rem + 2vw);
+    grid-template-columns: repeat(3, minmax(calc(8rem + 8vw), 1fr));
+    grid-gap: calc(0.5rem + 2vw);
+    position: absolute;    
+`
+const BGContainer= styled.div`
+    position: absolute;    
+    top: ${props => props.top};
+    bottom: ${props => props.bottom};
+    left: ${props => props.left};
+    right: ${props => props.top};
 `
 
 // Framer-motion Configurations
@@ -51,16 +97,31 @@ const container= {
     }
 }
 
+
 const ExperiencePage = () => {
 
-    const [numbers, setNumbers]= useState(0);
+    const scrollRef = useRef();
 
-    useEffect(() => {
-        let num= (window.innerHeight - 70)/30;
-        setNumbers(parseInt(num));
-    }, [])
+    const onScroll = () => {        
+        if (scrollRef.current) {
+            var { scrollTop, scrollHeight } = scrollRef.current;
+
+            if (scrollTop >= scrollHeight/3) {
+                    setTimeout (5000);
+                    document.getElementById('cont-1').style.zIndex= '0';
+                    document.getElementById('cont-2').style.zIndex= '1';                
+            }
+            else{
+                setTimeout (5000);
+                document.getElementById('cont-1').style.zIndex= '1';
+                document.getElementById('cont-2').style.zIndex= '0';
+            }
+        }
+    };
+    
 
     return (
+
         <MainContainer 
             variants={container} 
             initial= 'hidden' 
@@ -68,13 +129,37 @@ const ExperiencePage = () => {
             exit= {{
                 opacity: 0, transition: { duration: 0.5 }
             }}
-        >
-            <Container>
+            onScroll={() => onScroll()}
+            ref={scrollRef}
+        >     
+
+            <Container1 id='cont-1'>    
                 <LogoComponent  />
                 <HomeButton />
                 <SocialIcons />
-                <AnchorComponent numbers= {numbers}/>
-                <Center>
+
+                <Center className='center-edu'>
+                    <Grid>
+                        {
+                            Education.map(exp => {
+                                return <EduComponent key= {exp.id} exp= {exp} />
+                            })
+                        }
+                    </Grid>
+                </Center>
+                
+                <BGContainer top= '5rem' left= '5rem'>
+                    <BackgroundTitle text= "EDUCATION" top= '5rem' left= '5rem' />
+                </BGContainer>
+
+            </Container1>
+
+            <Container2 id='cont-2'>
+                <LogoComponent  />
+                <HomeButton />
+                <SocialIcons />
+
+                <Center className='center-exp'>
                     <Grid>
                         {
                             Experience.map(exp => {
@@ -83,10 +168,13 @@ const ExperiencePage = () => {
                         }
                     </Grid>
                 </Center>
-                
-                <BackgroundTitle text= "EXPERIENCE" bottom= '3rem'  right= '5rem' />
 
-            </Container>
+                <BGContainer bottom= '2rem' right= '5rem' >
+                    <BackgroundTitle text= "EXPERIENCE" bottom= '2rem' right= '5rem' />
+                </BGContainer>
+                
+            </Container2>
+            
         </MainContainer>
     )
 }
