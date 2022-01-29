f76b9318f270491a6aed9009b6871efc2139fb93
Hritam Dutta <hritamloyola10@gmail.com>
2022-01-29 15:44:21 +0530

    Changed Theme Palette

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,19 +1,19 @@
 // Creating dark & light themed variables
 
 export const lightTheme= {
-    body:"#FCF6F4",
-    text:"#000000",
-    fontFamily:"'Source Sans Pro', sans-serif",
-    bodyRgba : "252, 246, 244",
-    textRgba:"0,0,0"
+    body: "#F7FBFD",
+    text: "#00000E",
+    fontFamily: "'Source Sans Pro', sans-serif",
+    bodyRgba : "247, 251, 253",
+    textRgba: "0, 0, 14"
 }
 
 export const DarkTheme= {
-    body:"#000000",
-    text:"#FCF6F4",
-    fontFamily:"'Source Sans Pro', sans-serif",
-    textRgba : "252, 246, 244",
-    bodyRgba:"0,0,0"
+    body: "#00000E",
+    text: "#F7FBFD",
+    fontFamily: "'Source Sans Pro', sans-serif",
+    textRgba : "247, 251, 253",
+    bodyRgba: "0, 0, 14"
 }
 
 // Media Queries (in em)
