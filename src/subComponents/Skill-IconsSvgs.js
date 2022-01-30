2a497a212d3bf75b22f96d9aec311dbce218c609
Hritam Dutta <hritamloyola10@gmail.com>
2022-01-30 17:44:59 +0530

    Vercel Icon Added

--- src/subComponents/Skill-IconsSvgs.js
+++ src/subComponents/Skill-IconsSvgs.js
@@ -572,4 +572,17 @@ export const Heroku = (props) => (
     />
   </svg>
 )
+
+export const Vercel = (props) => (
+  <svg
+    viewBox="0 -17 256 256"
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="xMidYMid"
+    {...props}
+  >
+    <path 
+      fill= {props.fill}
+      d="m128 0 128 221.705H0z" />
+  </svg>
+)
   
