930fb6d4e804888b703c4811ea3c1a1b1b167ab2
Hritam Dutta <hritamloyola10@gmail.com>
2022-02-02 16:11:41 +0530

    BrowserRouter Issue Fixed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { BrowserRouter } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import '../node_modules/normalize.css'
 import { hydrate, render } from "react-dom"
 
-
-const APP= (
-  <React.StrictMode>    
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>    
-  </React.StrictMode>
+const APP= ( 
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>   
 );
  
 const rootElement = document.getElementById("root");
