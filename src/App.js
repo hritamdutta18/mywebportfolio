470d6dc8184bfffadee5c23631d80ce8e03a865c
Hritam Dutta <hritamloyola10@gmail.com>
2022-02-03 13:42:04 +0530

    Added 404 Error Page Route & SoundBar Conditional Display

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Route, Switch, useLocation } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 import { lightTheme } from "./components/Themes"
 import GlobalStyles from "./globalStyles"
-import { lazy, Suspense } from "react"
+import { lazy, Suspense, useEffect, useState } from "react"
 import { AnimatePresence } from "framer-motion"
 import PreLoader from "./subComponents/PreLoader"
 
@@ -12,28 +12,47 @@ const AboutPage = lazy(() => import("./components/AboutPage"));
 const ExpEduPage = lazy(() => import("./components/ExpEduPage"));
 const SkillsPage = lazy(() => import("./components/SkillsPage"));
 const ProjectPage = lazy(() => import("./components/ProjectPage"));
+const NotFound404 = lazy(() => import("./components/NotFound404"));
 const SoundBar = lazy(() => import("./subComponents/SoundBar"));
 
 function App() {
 
+  const [display, setDisplay]= useState(true);
   const location= useLocation ();
+
+  useEffect(() => {
+    const path= location.pathname;
+    const pathArray= ["/", "/about", "/education-experience", "/projects", "/skills"];
+    const error404 = route => { return route === path };
+
+    setDisplay(!(pathArray.find(error404) === undefined));
+  }, []);
 
   return(
     <>
       <GlobalStyles />
       <ThemeProvider theme= { lightTheme }>
         <Suspense fallback= {<PreLoader />}>
-          <SoundBar />
+          {
+            display ? 
+            <SoundBar />
+            :
+            <></>
+          }          
 
           {/* For framer-motion animation on page change */}
           <AnimatePresence exitBeforeEnter>
+
             <Switch location= {location} key= {location.pathname}>
               <Route exact path= "/" component= { Main } />
               <Route exact path= "/about" component= { AboutPage } />
               <Route exact path= "/education-experience" component= { ExpEduPage } />
               <Route exact path= "/projects" component= { ProjectPage } />
               <Route exact path= "/skills" component= { SkillsPage } />
+
+              <Route component= { NotFound404 } />
             </Switch>
+
           </AnimatePresence>  
           
         </Suspense>
@@ -43,4 +62,3 @@ function App() {
 }
 
 export default App
-
