fb08a4fc92eeaabc110b17f78107256aa9ef0a2d
Hritam Dutta <55477675+hritamdutta18@users.noreply.github.com>
2023-08-04 03:01:29 +0530

    Updated ProjectData.js

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -2,13 +2,21 @@ export const Project = [
     {
         id:1,
         name:"Ethereum-based E-Voting Web Application",
+        description:"A system that helps track the movement/exchange of materials via gate-pass, acts as a digital receipt for the complete transaction.",
+        tags:["react", "html", "css", "javascript"],        
+        demo:"https://gatepass-hritamdutta18.vercel.app/",
+        github:"https://github.com/hritamdutta18/gatepass"
+    },
+    {
+        id:2,
+        name:"Ethereum-based E-Voting Web Application",
         description:"A decentralized Ethereum-based e-voting system using Aadhaar number & OTP authentication.",
         tags:["react", "node", "express", "ganache", "truffle", "metamask", "solidity", "smart-contracts", "web3"],        
         demo:"",
         github:"https://github.com/hritamdutta18/Ethereum-based-E-Voting-WebApp"
     },
     {
-        id:2,
+        id:3,
         name:"Lane Prediction Optimization in VANET",
         description:"A study on the various existing approaches to overcome the problems on the general VANET network and an approach which could help to improve the overall efficiency of the VANET.",
         tags:["vanet", "sumo", "ns-3.34", "openstreetmaps"],        
@@ -16,7 +24,7 @@ export const Project = [
         github:""
     },
     {
-        id:3,
+        id:4,
         name:"BeeResume - An Automated Resume Builder",
         description:"BeeResume gives the user an automatically generated pre-designed resume, using a Multi-Step form.",
         tags:["react","bootstrap","node"],        
@@ -24,7 +32,7 @@ export const Project = [
         github:"https://github.com/hritamdutta18/bee-resume"
     },
     {
-        id:4,
+        id:5,
         name:"Diary",
         description:"A globally accessible personal diary that the writer can access and edit from anywhere.",
         tags:["react","node","mongodb","bootstrap","express"],   
@@ -33,7 +41,7 @@ export const Project = [
         github:"https://github.com/abhitcr1/IWPproject"        
     },
     {
-        id:5,
+        id:6,
         name:"SGGES '21 Website",
         description:"Website for the Virtual International Conference on Smart Grid and Green Energy Systems (SGGES '21), organized by VIT Vellore.",
         tags:["html","css","bootstrap","javascript"],        
@@ -41,7 +49,7 @@ export const Project = [
         github:"https://github.com/ieee-ias-vit/SGGES-21/"
     },
     {
-        id:6,
+        id:7,
         name:"IEEE IAS Website",
         description:"Official website of IEEE IAS VIT Vellore, made by members of the chapter.",
         tags:["html","css","bootstrap","javascript","firebase"],        
@@ -49,7 +57,7 @@ export const Project = [
         github:"https://github.com/hritamdutta18/IEEE-IAS-Website-2021"       
     },
     {
-        id:7,
+        id:8,
         name:"ZooMath",
         description:"ZooMath is a fun learning based web application that allows children to learn and practice multiplication tables and attend quizzes based on them.",
         tags:["html","css","javascript","bootstrap"],        
@@ -57,7 +65,7 @@ export const Project = [
         github:"https://github.com/hritamdutta18/Hackulus_Team_DASH"        
     },
     {
-        id:8,
+        id:9,
         name:"Diary in C",
         description:"A simple personal diary application made using C language.",
         tags:["c","cmd"],        
@@ -65,7 +73,7 @@ export const Project = [
         github:"https://github.com/hritamdutta18/diary"        
     },  
     {
-        id:9,
+        id:10,
         name:"CoviPrec",
         description:"This is my first ever website ! It enlists a few articles & precautions regarding COVID-19, along with a form for contact details. No back-end functionalities are included.",
         tags:["html","css","javascript"],        
